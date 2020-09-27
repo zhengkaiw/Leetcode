1eba0ac21010747f82b3ae76a3b8744a89b51e0c
kevin zheng <kevinzheng@kevins-MacBook-Pro.local>
2020-09-27 00:07:16 -0700

    20 updated

--- 20.valid-parentheses.java
+++ 20.valid-parentheses.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /*
  * @lc app=leetcode id=20 lang=java
  *
@@ -7,7 +9,15 @@
 // @lc code=start
 class Solution {
     public boolean isValid(String s) {
-        
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') stack.push(c);
+            else if (stack.isEmpty()) return false;
+            else if (c == ')' && !stack.isEmpty() && stack.pop() != '(') return false;
+            else if (c == '}' && !stack.isEmpty() && stack.pop() != '{') return false;
+            else if (c == ']' && !stack.isEmpty() && stack.pop() != '[') return false;
+        }
+        return stack.empty();
     }
 }
 // @lc code=end
