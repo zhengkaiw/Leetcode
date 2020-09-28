dd41b7e811321c02e6d9cf722598d2a3bc166666
kevin zheng <kevinzheng@kevins-MacBook-Pro.local>
2020-09-28 16:22:25 -0700

    203

---
+++ 203.remove-linked-list-elements.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=203 lang=java
+ *
+ * [203] Remove Linked List Elements
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy, cur = head;
+        while (cur != null) {
+            if (cur.val == val) {
+                prev.next = cur.next;
+            } else prev = cur;
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+}
+// @lc code=end
+
